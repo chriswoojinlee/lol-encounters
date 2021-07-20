802a55b39dcbc421eee15d93c956f2b95959ffa2
chriswoojinlee <chrislee707@gmail.com>
2021-07-20 00:07:02 -0600

    Set up Maven and successfully connected to Riot API

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,6 @@ public class main {
     public static void main(String args[]) {
         Orianna.Configuration config = new Orianna.Configuration();
         Orianna.loadConfiguration(config);
-        Orianna.setRiotAPIKey("RGAPI-54494af6-09cb-415e-804f-3af3bd4b4df6");
         config.setDefaultPlatform(Platform.NORTH_AMERICA);
         Orianna.setDefaultLocale("en_US");
     }
